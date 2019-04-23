76137c8557cd11c8669d9f7966c4647f3cf452c4
Labayanne <yannblanchet@orange.fr>
2019-04-23 19:23:47 +0200

    Resolution bug && clean code

--- game/Game.py
+++ game/Game.py
@@ -8,39 +8,72 @@ class Game :
 
     def __init__(self) :
 
-        self.prefix = ["Parc"];
+        self.path_base = "world/Parc/"
+        self.path_place = ""
+        self.prefix = []
         self.user_name = "TOTO"
+        self.nb_place_append = 0
+
 
     def tick(self) :
 
-        path_place = self.prefix[0]
+        self.path_place = self.path_base
 
         while(True):
-            input_string = input(self.user_name + "@:" + path_place + "$ ")
-            #print(self.prefix)
-            nb_place_append = 0
 
-            if ("cd" in input_string):
-                path = (input_string.split(" ")[1]).split("/")
-                for place in path:
-                    if(place == ".."):
+            input_string = input(self.user_name + "@:" + self.path_place + "$ ")
+            
+            self.nb_place_append = 0
+
+            input_array = input_string.split(" && ")
+
+            for input_string in input_array:
+
+                input_string = self.parse_input(input_string)
+
+                input_string = self.build_input(input_string)
+
+                if(os.system(input_string) != 0):
+                    for i in range(self.nb_place_append):
                         self.prefix.pop()
-                    else:
-                        self.prefix.append(place)
-                        nb_place_append += 1
-                input_string = ""
 
-            else:
-                input_string = " && " + input_string
 
-            path_place = ""
-            for place in self.prefix:
-                path_place += place + "/"
-            #print(path_place)
-            input_string = "cd " + path_place + input_string
+    def parse_input(self, input_string):
 
-            if(os.system(input_string) != 0):
-                for i in range(nb_place_append):
+        if (input_string.startswith("cd")):
+
+            input_string = self.cd(input_string)
+
+        else:
+
+            input_string = " && " + input_string
+
+        return input_string
+
+
+    def build_input(self, input_string):
+
+        self.path_place = self.path_base
+                
+        for place in self.prefix:
+            self.path_place += place + "/"
+        
+        return "cd " + self.path_place + input_string
+
+
+    def cd(self, input_string):
+
+        path = (input_string.split(" ")[1]).split("/")
+
+        for place in path:
+
+            if(place == ".."):
+                if(len(self.prefix) > 0):
                     self.prefix.pop()
+            else:
+                self.prefix.append(place)
+                self.nb_place_append += 1
+
+        return ""
 
             
--- game/world/Parc/Cat.py
+++ game/world/Parc/Cat.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
-from ...Player import *
+import os
+import sys
 
+directory = os.path.dirname(os.path.abspath(__file__))
+ 
+while not directory.endswith('game'):
+    directory = os.path.dirname(directory)
+ 
+if directory not in sys.path:
+    sys.path.append(directory)
+
+from Player import *
+
+player = Player()
 print("C'est un chat")
 player.say("Hello")
