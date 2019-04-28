3bc3e845487f72967909787be75d95e89e6efc64
chocorion <chocorion@protonmail.com>
2019-04-28 16:42:06 +0200

    ws works

--- frontend.js
+++ frontend.js
@@ -24,7 +24,7 @@
   }
 
   // open connection
-  var connection = new WebSocket('http://universitus.rhythmgamers.net:1337');
+  var connection = new WebSocket('ws://universitus.rhythmgamers.net:1337');
 
 
 
