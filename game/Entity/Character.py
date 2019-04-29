a375e003a053122e2d95948f20dec0fa899e2eca
Labayanne <yannblanchet@orange.fr>
2019-04-29 11:23:17 +0200

    NEW LINES

--- game/Entity/Character.py
+++ game/Entity/Character.py
@@ -43,7 +43,7 @@ class Character(Entity):
             self.writeIn("\t\tprint(u\""+self.color+self.name+":\u001b[37m \", end = '')")
             self.writeIn("\t\tfor line in thinkingAbout[0].split('\\n'):")
             self.writeIn("\t\t\tprint(u\"\u001b[37m\"+line)")
-            self.writeIn("\t\t\tprint(\"\\n\")")
+            self.writeIn("\t\t\tprint(\"\\n\\n\")")
             self.writeIn("\t\tprint(\"\\n\")")
             self.writeIn("\t\tr = 1")
             self.writeIn("\t\tfor choice,reponses in thinkingAbout[1].items():")
