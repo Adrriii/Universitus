71d341aa10a8525d3c93b782215d869c9fcb53c7
Adri- <adrien.boitelle@gmail.com>
2019-05-01 00:35:55 +0200

    remove cmd

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ var optsc = {
     'OpenStdin': true,
     'StdinOnce': false,
     'Env': null,
-    'Cmd': ['storage-opt "size=50MB"'],
+    'Cmd': [],
     'Dns': ['8.8.8.8', '8.8.4.4'],
     'Image': 'docker-game',
     'Volumes': {},
