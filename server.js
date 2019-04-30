7720642ecae09874ea94e92ffb0111b03b8f0599
Adri <adrien.boitelle@gmail.com>
2019-04-30 13:58:28 +0200

    remove useless comments and fix status index on close

--- server.js
+++ server.js
@@ -63,48 +63,19 @@ function sendMessage(index, text, password = false) {
 
 /**
  * HTTP server
+ * Base server on which the WebSocket server will run
  */
-var server = http.createServer(function (request, response) {
-    // Not important for us. We're writing WebSocket server,
-    // not HTTP server
-});
+var server = http.createServer(function (request, response) {});
 server.listen(webSocketsServerPort, function () {
     console.log((new Date()) + " Server is listening on port "
         + webSocketsServerPort);
 });
-
-// function exec(container, cmd) {
-
-//     let s = new Stream.PassThrough();
-
-//     var options = {
-//         Cmd: ['bash', '-c', cmd],
-//         Env: [],
-//         AttachStdout: true,
-//         AttachStderr: true
-//     };
-
-//     container.exec(options, function (err, exec) {
-//         if (err) return;
-//         exec.start(function (err, stream) {
-//             if (err) return;
-
-//             container.modem.demuxStream(stream, s, s);
-
-//         });
-//     });
-
-//     return s;
-// }
 
 
 /**
  * WebSocket server
  */
 var wsServer = new webSocketServer({
-    // WebSocket server is tied to a HTTP server. WebSocket
-    // request is just an enhanced HTTP request. For more info 
-    // http://tools.ietf.org/html/rfc6455#page-6
     httpServer: server
 });
 
@@ -180,11 +151,8 @@ async function createContainer(userName, index) {
 wsServer.on('request', function (request) {
     console.log((new Date()) + ' Connection from origin ' + request.origin + '.');
 
-    // accept connection - you should check 'request.origin' to
-    // make sure that client is connecting from your website
-    // (http://en.wikipedia.org/wiki/Same_origin_policy)
     var connection = request.accept(null, request.origin);
-    // we need to know client index to remove them on 'close' event
+    
     var index = clients.push(connection) - 1;
 
     // potentially initialize the client
@@ -194,13 +162,12 @@ wsServer.on('request', function (request) {
 
     var userName = false;
 
-    console.log((new Date()) + ' Connection accepted.');
+    console.log((new Date()) + ' Connection accepted with index '+index);
     sendMessage(index, "Enter your login : ");
 
     // user sent some message
     connection.on('message', async function (message) {
-        if (message.type === 'utf8') { // accept only text
-            // first message sent by user is their name
+        if (message.type === 'utf8') {
 
             switch (clients_status[index]) {
                 case status.INIT:
@@ -295,6 +262,7 @@ wsServer.on('request', function (request) {
 
             // remove user from the list of connected clients
             clients.splice(index, 1);
+            clients_status.splice(index, 1);
         }
     });
 });
